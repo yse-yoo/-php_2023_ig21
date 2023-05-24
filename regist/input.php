bd1cdc979da72cd1638643d580338d98003a00bc
yse-yoo <youhei_yoshikawa@yse-c.net>
2023-05-24 11:03:24 +0900

    new project

---
+++ regist/input.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=2.0">
+    <title>Regist</title>
+</head>
+<body>
+    <h1>入力画面</h1>
+    <form action="" method="post">
+        <div>
+            <label for="">氏名</label>
+            <input type="text" name="name">
+        </div>
+        <div>
+            <label for="">Email</label>
+            <input type="text" name="email">
+        </div>
+        <div>
+            <label for="">パスワード</label>
+            <input type="password" name="password">
+        </div>
+        <button>確認</button>
+    </form>
+</body>
+</html>
